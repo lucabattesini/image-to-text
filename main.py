1a4872945e017335348a5ff5402672300819267d
Luca Battesini <lucabattesini1@gmail.com>
2025-07-03 15:06:26 -0400

    Creating a structure which will write all the text in a txt file

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import ocrmypdf
+import pdfplumber
 
-input_pdf = "files/input.pdf"
-output_pdf = "result/output.pdf"
+input_pdf = "files/input2.pdf"
+ocr_pdf = "files/output.pdf"
+formated_text = "result/final_version.txt"
 
-ocrmypdf.ocr(input_pdf, output_pdf, language='por')
+ocrmypdf.ocr(input_pdf, ocr_pdf, language='por')
+
+extracted_text = ""
+with pdfplumber.open(ocr_pdf) as pdf:
+    for page in pdf.pages:
+        text = page.extract_text(layout=True)
+        if text:
+            extracted_text += text + "\n"
+
+with open(formated_text, "w", encoding="utf-8") as f:
+    f.write(extracted_text)
